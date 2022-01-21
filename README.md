85158f09b302967b3a122eec411abeaddcc682d4
GizmoTechy <windrider786@gmail.com>
2022-01-20 19:03:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rename-Sequential-Files
+
+A small rename function I created to handle documents that have the same file name during automated copy jobs. This function will allow the files to be copied by just appending a (#) to the end of the file name similar to how file systems do so. 
---
+++ rename.py
@@ -0,0 +1,18 @@
+import re
+
+
+def Rename(new_directory, current_file_name, file_extension):
+    regex_search = re.search('(\([0-9]+\))', current_file_name)
+    if regex_search:
+        file_num = re.sub('[()]', '', regex_search.group(0))
+        file_name_no_num = re.sub('[()]', '', re.sub(regex_search.group(0), '', current_file_name)).strip()
+        new_num = int(file_num) + 1
+        new_file_name = f'{file_name_no_num} ({new_num})'
+    else:
+        new_file_name = f'{current_file_name} (1)'
+
+    if new_directory.joinpath(f'{new_file_name}{file_extension}').exists():
+        return Rename(new_directory, new_file_name, file_extension)
+    else:
+        return f'{new_file_name}{file_extension}'
+
