29ae85803fdc64da6347f9eceab99dc79f64bff0
GizmoTechy <windrider786@gmail.com>
2022-04-11 11:52:09 -0400

    Update rename.py
    Fixed an issue where the default behavior was to add (1) even if no other files existed.

--- rename.py
+++ rename.py
@@ -2,14 +2,17 @@ import re
 
 
 def Rename(new_directory, current_file_name, file_extension):
-    regex_search = re.search('(\([0-9]+\))', current_file_name)
-    if regex_search:
-        file_num = re.sub('[()]', '', regex_search.group(0))
-        file_name_no_num = re.sub('[()]', '', re.sub(regex_search.group(0), '', current_file_name)).strip()
-        new_num = int(file_num) + 1
-        new_file_name = f'{file_name_no_num} ({new_num})'
+    regex_search = re.search(r'(\([0-9]+\))', current_file_name)
+    if new_directory.joinpath(f'{current_file_name}{file_extension}').exists():
+        if regex_search:
+            file_num = re.sub('[()]', '', regex_search.group(0))
+            file_name_no_num = re.sub('[()]', '', re.sub(regex_search.group(0), '', current_file_name)).strip()
+            new_num = int(file_num) + 1
+            new_file_name = f'{file_name_no_num} ({new_num})'
+        else:
+            new_file_name = f'{current_file_name} (1)'
     else:
-        new_file_name = f'{current_file_name} (1)'
+        return f'{current_file_name}{file_extension}'
 
     if new_directory.joinpath(f'{new_file_name}{file_extension}').exists():
         return Rename(new_directory, new_file_name, file_extension)
